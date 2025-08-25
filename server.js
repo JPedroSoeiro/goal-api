2b695b77b50d7fe1b7e22ae1ea6221c306bd9a37
Pedro Henrique <80182753+Peedrohenrique@users.noreply.github.com>
2025-08-25 10:46:25 -0300

    FIX: CORRECAO DE CORS

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
 // Importe o Swagger
-const swaggerUi = require('swagger-ui-express');
-const swaggerSpec = require('./swagger');
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./swagger");
 
 // Suas rotas
 const router = require("./routes");
@@ -14,11 +14,17 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // Permite todas as origens
+    methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
+    allowedHeaders: ["Content-Type", "Authorization"], // Cabeçalhos permitidos
+  })
+);
 app.use(bodyParser.json());
 
 // Rota para a documentação do Swagger
-app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use("/api", router);
 
@@ -29,5 +35,7 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`API de back-end rodando em http://localhost:${PORT}`);
-  console.log(`Documentação do Swagger disponível em http://localhost:${PORT}/api/docs`);
+  console.log(
+    `Documentação do Swagger disponível em http://localhost:${PORT}/api/docs`
+  );
 });
